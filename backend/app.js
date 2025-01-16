1ec2f23660bf060c8d3466f5d5c4255736f76616
dev-VanessaMahado <dev.vanessamachado@gmail.com>
2025-01-16 20:37:21 -0300

    adicionada função para avaliar pedido

--- backend/app.js
+++ backend/app.js
@@ -4,10 +4,11 @@ const usuarioRoutes = require('./routes/UsuarioRoute');
 const pedidoRoutes = require('./routes/PedidoRoute'); // Importando as rotas de pedidos
 const estudanteRoutes = require('./routes/estudanteRoute');
 const avaliadorRoutes = require('./routes/avaliadorRoute');
+const avaliacaoRoutes = require('./routes/avaliacaoRoute'); // Importando as rotas de avaliações
 
 const app = express();
 
-// Middleware para analisar o corpo JSON
+
 app.use(express.json());
 
 // Middleware para garantir que as respostas sejam enviadas com codificação UTF-8
@@ -22,10 +23,10 @@ app.use('/usuarios', usuarioRoutes);
 app.use('/estudantes', estudanteRoutes);
 // Rotas de avaliadores
 app.use('/avaliadores', avaliadorRoutes);
-
 // Rotas de pedidos
-app.use('/pedidos', pedidoRoutes); // Rotas para pedidos
-
+app.use('/pedidos', pedidoRoutes); 
+// Rotas de avaliações - Em pedidos, pois após avaliação o pedido deve ser diretamente atualizado e o pedido original sobrescrito
+app.use('/pedidos', avaliacaoRoutes); 
 // Middleware para rotas não encontradas
 app.use((req, res, next) => {
     res.status(404).json({ error: 'Rota não encontrada.' });
