9f737936de633bbde43dc77dd5a84ffa8cfca2bd
Carlos Neto <neto.mossel12@gmail.com>
2025-01-15 21:04:22 -0300

    feat: inclusão de Avaliador

--- backend/app.js
+++ backend/app.js
@@ -1,14 +1,15 @@
 const express = require('express');
 const usuarioRoutes = require('./routes/usuarioRoute');
 const estudanteRoutes = require('./routes/estudanteRoute');
+const avaliadorRoutes = require('./routes/avaliadorRoute');
 
 const app = express();
 
 app.use(express.json());
 
-
 app.use('/usuario', usuarioRoutes);
 app.use('/estudante', estudanteRoutes);
+app.use('/avaliador', avaliadorRoutes);
 
 app.use((req, res, next) => {
     res.status(404).json({ error: 'Rota não encontrada.' });
