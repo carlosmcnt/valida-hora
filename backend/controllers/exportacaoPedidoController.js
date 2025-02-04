2d671e75b38474b95d6c1c61ae5ba6d0bf5c1d12
dev-VanessaMahado <dev.vanessamachado@gmail.com>
2025-02-04 17:54:59 -0300

    melhorias e exportação e download de relatório de pedido aprovado

--- backend/controllers/exportacaoPedidoController.js
+++ backend/controllers/exportacaoPedidoController.js
@@ -3,22 +3,32 @@ const Exportacao = require('../models/ExportacaoPedido');
 require('dotenv').config();
 
 class ExportacaoController {
-
+    
     async exportarPedidos(req, res) {
         try {
             const { id_usuario } = req.params;
-            const caminhoArquivo = await ExportacaoService.gerarArquivoTxt(id_usuario);
+            const caminhoArquivo = await ExportacaoService.gerarArquivoTxtPorUsuario(id_usuario);
             res.download(caminhoArquivo);
         } catch (error) {
             res.status(400).json({ message: error.message });
         }
     }
 
+   
+    async exportarPedidoPorId(req, res) {
+        try {
+            const { id_pedido } = req.params;
+            const caminhoArquivo = await ExportacaoService.gerarArquivoTxtPorPedido(id_pedido);
+            res.download(caminhoArquivo);
+        } catch (error) {
+            res.status(400).json({ message: error.message });
+        }
+    }
+
+    
     async baixarArquivo(req, res) {
         try {
             const { id_usuario } = req.params;
-
-            
             const result = await Exportacao.buscarArquivoPorUsuario(id_usuario);
 
             if (result.length === 0) {
@@ -26,7 +36,6 @@ class ExportacaoController {
             }
 
             const { nome_arquivo, conteudo } = result[0];
-
             res.setHeader('Content-Disposition', `attachment; filename=${nome_arquivo}`);
             res.setHeader('Content-Type', 'text/plain; charset=utf-8');
             res.send(conteudo);
@@ -34,6 +43,29 @@ class ExportacaoController {
             res.status(500).json({ message: 'Erro ao baixar o arquivo.', error: error.message });
         }
     }
+
+    
+    async baixarArquivoPorPedido(req, res) {
+        try {
+            const { id_pedido } = req.params;
+            const result = await Exportacao.buscarPedidoAprovadoPorId(id_pedido);
+    
+            if (result.length === 0) {
+                return res.status(404).json({ message: 'Nenhum arquivo encontrado para esse pedido.' });
+            }
+    
+            const { nome_arquivo, conteudo } = result[0];
+    
+            res.setHeader('Content-Disposition', `attachment; filename=${nome_arquivo}`);
+            res.setHeader('Content-Type', 'text/plain; charset=utf-8');
+            res.send(conteudo);
+        } catch (error) {
+            console.error(error);
+            res.status(500).json({ message: 'Erro ao baixar o arquivo do pedido.', error: error.message });
+        }
+    }
+    
 }
 
 module.exports = new ExportacaoController();
+
